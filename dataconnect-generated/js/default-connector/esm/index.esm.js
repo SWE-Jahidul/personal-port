0b09cb5093b43be445ebb9e13f9991b3cb0f064e
SWE-Jahidul <programmerjahid162@gmail.com>
2025-08-19 17:25:22 +0600

    Updated: Code

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'portfolio-template',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'portfolio-template',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
